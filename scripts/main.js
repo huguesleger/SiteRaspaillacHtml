429cf80f9985e8b6715c20ceb37727c73dcf5917
huguesleger <hugueslegerdevweb@gmail.com>
2016-08-29 17:34:17 +0200

    jquery nav + resize photo slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
-$(window).on('scroll',function(){
-    if($(window).scrollTop()){
-        $('.main').addClass('navbar-min');
-    }else{
-        $('.main').removeClass('navbar-min');
-    }
+
+//Scroll Menu
+$(window).on('scroll', function(){
+	if( $(window).scrollTop()>100 ){
+		$('.navbar-main').addClass('navbar-min');
+		$('.navbar-main').addClass('navbar-brand-min');
+		$('.navbar-main').addClass('navbar-nav-min');
+		$('.navbar-main').removeClass('navbar-nav');
+			$('.navbar-main').removeClass('navbar-brand');
+
+	} else {
+		$('.navbar-main').removeClass('navbar-min');
+		$('.navbar-main').removeClass('navbar-brand-min');
+		$('.navbar-main').removeClass('navbar-nav-min');
+	}
+
 });
